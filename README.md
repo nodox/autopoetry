e64120f044f5aed37234bb10153fd058d987ed65
Steven Natera <steven@stevennatera.com>
2023-07-12 19:32:56 -1000

    add license and readme

--- README.md
+++ README.md
@@ -1,71 +1,39 @@
-# autopoetry README
+# Autopoetry: Automatically install missing dependencies with Python Poetry
 
-This is the README for your extension "autopoetry". After writing up a brief description, we recommend including the following sections.
+The most convenient way to install missing dependencies and automatically append the necessary import statements to the top of your Python files for projects using [Poetry](https://python-poetry.org/) as the dependency manager
+
+## Caveats
+
+- **Alpha Stage:** Please note that this extension is currently in the alpha stage, and there may be breaking changes at any time.
+- **Limited Testing:** While the extension has been designed to solve specific problems encountered during the development of single-file Python programs in new codebases, its behavior in large codebases, nested directories, or monorepos is uncertain.
+
+
+## Getting Started
+- Install extension.
+
+- Open a Python file in Visual Studio Code.
+
+- Ensure that your project has a valid Poetry configuration (`pyproject.toml`).
+
+- On save, if any missing dependencies are detected in your Python file, you will be prompted to install them. Confirm to proceed.
+
+- The necessary `import` statements will be automatically appended to the top of your file, helping you manage your dependencies more effectively.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
-
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+- Automatically installs missing dependencies for your Python programs using Poetry.
+- Appends the required import statements to the top of your Python files, making it easier to manage dependencies.
+- Streamlines the development process for single-file Python programs, especially in new codebases.
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+- This extension assumes that your project already has an existing Poetry setup.
 
-## Extension Settings
+## Feedback and Support
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+We appreciate your interest in this extension! As it is currently in alpha stage, your feedback and suggestions are valuable in helping us improve its functionality and reliability.
 
-For example:
+- **Twitter:** You can reach out to the extension's author, Steven Natera, on Twitter [@stevennatera](https://twitter.com/stevennatera).
+- **LinkedIn:** Connect with Steven Natera on LinkedIn at [linkedin.com/in/snatera](https://www.linkedin.com/in/snatera).
 
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+Feel free to report any bugs, issues, or feature requests using my social media handles.
--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,100 @@
-// help
-// https://john.colagioia.net/blog/2020/07/08/vscode.html
-
-
-
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as childProcess from 'child_process';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "autopoetry" is now active!');
+	let poetryAddDisposable = vscode.workspace.onDidSaveTextDocument(async (document: vscode.TextDocument) => {
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        
+        if (workspaceFolders && workspaceFolders.length > 0) {
+            const workspaceRoot = workspaceFolders[0].uri.fsPath;
+            const filePath = document.uri.fsPath;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('autopoetry.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from autopoetry!');
-	});
+            try {
+                const missingPackages = await findMissingPackages(filePath, workspaceRoot);
+                if (missingPackages.length > 0) {
+                    installPackages(missingPackages, workspaceRoot, document);
+                }
+            } catch (error) {
+                console.log("debug:missingPackages", error)
+            }
+        }
 
-	context.subscriptions.push(disposable);
-
-
-	let finder = vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
-        // const filePath = document.uri.fsPath;
-        // const missingPackages = findMissingPackages(filePath);
-
-        // if (missingPackages.length > 0) {
-        //     // installPackages(missingPackages);
-        // }
-
-		vscode.window.showInformationMessage('Finding filesss....');
     });
 
-    context.subscriptions.push(finder);
+    context.subscriptions.push(poetryAddDisposable);
+}
+
+function findMissingPackages(filePath: string, workspaceRoot: string): Promise<string[]> {
+    return new Promise<string[]>((resolve, reject) => {
+        const command = `pylint --disable=all --enable=undefined-variable --output-format=json ${filePath}`;
+
+        childProcess.exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.log('debug:childProcess', stdout)
+
+                const analysisResults = JSON.parse(stdout);
+                const missingPackages = analysisResults
+                    .map((result: any) => {
+
+                        const regex = /(?:Undefined variable ')([^']+)/;
+                        const match = result.message.match(regex);
+                        const extractedWord = match ? match[1] : null;
+                        // console.log(extractedWord)
+
+                        return extractedWord               
+                    });
+    
+                resolve(missingPackages);
+            }
+
+            reject(`Failed to analyze Python file`);
+            return;
+        });
+    });
+}
+
+
+function installPackages(packages: string[], workspaceRoot: string, document: vscode.TextDocument): void {
+    const installCommand = `cd ${workspaceRoot} && poetry add ${packages.join(' ')}`;
+    vscode.window.showInformationMessage(`Packages missing. Want to install with 'poetry add ${packages.join(' ')}'`, "Yes", "No")
+    .then(answer => {
+      if (answer === "Yes") {
+        childProcess.exec(installCommand, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Failed to install packages: ${error.message}`);
+            } else {
+                console.log(`Packages installed successfully: ${stdout}`);
+                for (const name of packages) {
+                    writeImportStatement(name, document);
+                }
+
+                vscode.window.showInformationMessage('autopoetry: Packages installed successfully.');
+            }
+        });
+
+      }
+    })
+}
+
+
+function writeImportStatement(name: string, document: vscode.TextDocument): void {
+    const contentToAppend = `import ${name}\n`;
+
+    // Create a TextEdit to modify the content
+    const edit = new vscode.TextEdit(
+        new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 0)),
+        contentToAppend
+    );
+
+    // Apply the TextEdit to the TextDocument
+    const workspaceEdit = new vscode.WorkspaceEdit();
+    workspaceEdit.set(document.uri, [edit]);
+    vscode.workspace.applyEdit(workspaceEdit);
 }
 
 // This method is called when your extension is deactivated
